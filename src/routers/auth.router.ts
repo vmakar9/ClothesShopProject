cde915c13fb0124cb615dd683c4300cb12529915
vmakar9 <zmerin212@gmail.com>
2023-06-02 19:30:31 +0300

    Register,Auth

--- src/routers/auth.router.ts
+++ src/routers/auth.router.ts
@@ -1,9 +1,10 @@
 import {Router} from "express";
 import {authController} from "../controllers/auth.controller";
+import {userMiddleware} from "../middleware/user.middleware";
 
 const router = Router();
 
-router.post('/login',authController.login)
+router.post('/login',userMiddleware.getDynamicallyOrThrow("email"),authController.login)
 router.post('/register',authController.register)
 
 export const authRouter = router
