5fbfff171e22310a35c178b56e3a7c7af1a21735
vmakar9 <zmerin212@gmail.com>
2023-06-27 15:49:24 +0300

    Check activated account

--- src/routers/messages.router.ts
+++ src/routers/messages.router.ts
@@ -1,19 +1,23 @@
 import {Router} from "express"
 import {authMiddleware} from "../middleware/auth.middleware";
 import {messagesController} from "../controllers/messages.controller";
+import {accessMiddleware} from "../middleware/access.middleware";
 
 const router = Router()
 
 router.get("/recivedmessages",
     authMiddleware.checkAccessToken,
+    accessMiddleware.getUserStatus,
     messagesController.get)
 
 router.post("/:recipient",
     authMiddleware.checkAccessToken,
+    accessMiddleware.getUserAccess,
     messagesController.sendMessage)
 
 router.get("/sendedmessages",
     authMiddleware.checkAccessToken,
+    accessMiddleware.getUserStatus,
     messagesController.sendersMessages)
 
 export const messagesRouter = router;
